4265c8646deeac9a557ba9920af43712b0ddf962
thebluebsss <167521513+thebluebsss@users.noreply.github.com>
2025-10-28 12:13:32 +0700

    add FE code

--- BE_Server-side/src/main.ts
+++ BE_Server-side/src/main.ts
@@ -7,29 +7,34 @@ import { AllExceptionsFilter } from './common/filters/all-exeption.filter';
 import { TransformResponseInterceptor } from './common/interceptor/transform-response.interceptor';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Cho phép frontend ở port 3001 gọi
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-    // Global Exception Filter
-    app.useGlobalFilters(new AllExceptionsFilter());
+  // Global Exception Filter
+  app.useGlobalFilters(new AllExceptionsFilter());
 
-    // Global Response Interceptor
-    app.useGlobalInterceptors(new TransformResponseInterceptor)
+  // Global Response Interceptor
+  app.useGlobalInterceptors(new TransformResponseInterceptor());
 
-    // Config
-    const configService = app.get(ConfigService)
+  // Config
+  const configService = app.get(ConfigService);
 
-    // API Documentation Swagger
-    const config = new DocumentBuilder()
-        .setTitle('The first NestJs project')
-        .setDescription('The API description')
-        .setVersion('1.0')
-        .addTag('User, Garden, Vegatable, Sale')
-        .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, documentFactory);
-    
-    app.useGlobalPipes(new ValidationPipe({ transform: true })); // Ensures proper validation and transformation
-    const port = configService.get<string>('PORT')
-    await app.listen(port ?? 3000);
+  // API Documentation Swagger
+  const config = new DocumentBuilder()
+    .setTitle('The first NestJs project')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('User, Garden, Vegatable, Sale')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // Ensures proper validation and transformation
+  const port = configService.get<string>('PORT');
+  await app.listen(port ?? 3000);
 }
 bootstrap();
