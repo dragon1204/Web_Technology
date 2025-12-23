c793afaac12fe24bcdd1f01a4e395724005c3abb
Ngọc Long Vũ <vungoclong20041204@gmail.com>
2025-12-23 20:42:44 +0700

    fix authorization

--- BE_Server-side/src/main.ts
+++ BE_Server-side/src/main.ts
@@ -25,7 +25,6 @@ async function bootstrap() {
   //Validate global
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
       transform: true,
       forbidNonWhitelisted: false,
       exceptionFactory: async (errors: ValidationError[]) => {
