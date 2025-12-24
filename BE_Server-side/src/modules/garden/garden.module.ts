91dfbbcefc9783ff5d6b3da4a8b4000886c1143d
cmanh75 <npcm752004t2k29@gmail.com>
2025-12-24 15:45:03 +0700

    fix garden controller import path

--- BE_Server-side/src/modules/garden/garden.module.ts
+++ BE_Server-side/src/modules/garden/garden.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { GardenService } from './garden.service';
 import { SaleController } from './sale/sale.controller';
 import { SaleService } from './sale/sale.service';
-import { GardenController } from './garden.controler';
+import { GardenController } from './garden.controller';
 
 @Module({
   controllers: [GardenController, SaleController],
